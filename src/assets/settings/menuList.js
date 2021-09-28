59abcae8ee32794c3b99f8666f3804f2f47191f1
Upkeep <upkeep@airmail.cc>
2021-09-28 18:46:42 +0200

    init

---
+++ src/assets/settings/menuList.js
@@ -0,0 +1,38 @@
+export default [
+
+    {
+        header: "User Settings",
+        content: [
+            {
+                title: "Profile",
+                type: "submenu",
+                to: "profile"
+            },
+            {
+                title: "Privacy",
+                type: "submenu",
+                to: "privacy"
+            },
+        ]
+    },
+    {
+        content: [
+            {
+                title: "Supported XEPs",
+                type: "submenu",
+                to: "XEPs"
+            },
+            {
+                title: "About",
+                type: "submenu",
+                to: "about"
+            },
+            {
+                title: "Log Out",
+                type: "nuxt",
+                to: "/logout",
+                color: "#fb4934",
+            }
+        ]
+    }
+];
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const themeColors = require("./colors.js");
+
+module.exports = {
+    purge: {
+    enabled: process.env.NODE_ENV !== "development",
+        content: [
+            './src/**/*.html',
+            './src/**/*.vue',
+            './src/**/*.jsx',
+        ],
+    },
+    theme: {
+        colors: {
+            ...themeColors
+        }
+    },
+    variants: {
+        extend: {
+            borderRadius: ['hover', 'focus'],
+            backgroundColor: ['active'],
+            textColor: ['active'],
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+}
