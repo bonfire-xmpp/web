c102c545f8a8de79a18339bda29e2d5786096130
Upkeep <upkeep@airmail.cc>
2021-10-08 23:04:28 +0200

    chore: patch to work with latest core

--- src/api.worker.ts
+++ src/api.worker.ts
@@ -1,13 +1,6 @@
-import { Bridge } from "@bonfire-xmpp/apibridge";
-import core, { API, Events } from "@bonfire-xmpp/core";
+import bridge from "@bonfire-xmpp/core";
 
 const ctx = self as unknown as Worker;
-
-const bridge = new Bridge<unknown, unknown, API, Events>();
-
-bridge.define({
-  ...core({ bridge }),
-});
 
 ctx.onmessage = (e: MessageEvent) => bridge.feed(e.data);
 bridge.onsend = (mesg: Array<unknown>) => ctx.postMessage(mesg);
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
-import { Bridge } from "@bonfire-xmpp/apibridge";
-import type { API, Events } from "@bonfire-xmpp/core";
+import bridge from "@bonfire-xmpp/core/src/index.view";
 
 const apiWorker = new Worker(new URL("@/api.worker.ts", import.meta.url));
-const api = new Bridge<API, Events>();
-apiWorker.onmessage = (e: MessageEvent) => api.feed(e.data);
-api.onsend = (mesg: Array<unknown>) => apiWorker.postMessage(mesg);
+apiWorker.onmessage = (e: MessageEvent) => bridge.feed(e.data);
+bridge.onsend = (mesg: Array<unknown>) => apiWorker.postMessage(mesg);
 
 const app = createApp(App);
 app.use((app) => {
-  app.config.globalProperties.$api = api;
+  app.config.globalProperties.$api = bridge;
 });
 app.mount("#app");
